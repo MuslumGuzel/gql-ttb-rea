1083b190e99bc81190c6b64468c0a7719fb64668
Müslüm GÜZEL <guzel.muslum8@gmail.com>
2018-04-14 22:49:30 +0300

    Add player statistics

--- client/src/components/pages/clanMembers.js
+++ client/src/components/pages/clanMembers.js
@@ -1,51 +1,84 @@
-//https://api.worldoftanks.eu/wgn/clans/info/?application_id=8309ea87a6d621f5933f703e0b8a48fe&clan_id=500076553
+//https://api.worldoftanks.eu/wgn/clans/info/?application_id=8309ea87a6d621f5933
+//f703e0b8a48fe&clan_id=500076553
 
 import React from "react";
-import Axios from "axios"; 
+import Axios from "axios";
+import Config from "../../configuration/config";
 
 export default class CommentList extends React.Component {
   state = {
     members: []
   };
 
+  componentWillMount() {
 
-  componentDidMount() {
     Axios
-      //.get("https://api.worldoftanks.eu/wot/account/tanks/?application_id=demo&account_id=513944695")
-      .get("https://api.worldoftanks.eu/wgn/clans/info/?application_id=8309ea87a6d621f5933f703e0b8a48fe&clan_id=500076553")
+      .get(Config.webServices.clanWs, {
+      params: {
+        application_id: Config.webServices.applicationId,
+        clan_id: '500076553'
+      }
+    })
       .then(response => {
-          //let stringifiedResp = JSON.stringify(response.data);
-          
-        let members = response.data.data["500076553"].members;        
-        if (members != null) {
-          this.setState({members});
+        let members = response.data.data["500076553"].members;
+        let memberArray = [];
+
+        for (var i = 0; i < members.length; i++) {
+          let memberDetail = {};
+          memberDetail.account_name = members[i].account_name;
+          memberDetail.role_i18n = members[i].role_i18n;
+          memberDetail.account_id = members[i].account_id;
+
+          Axios
+            .get(Config.webServices.accountWs, {
+            params: {
+              application_id: Config.webServices.applicationId,
+              account_id: memberDetail.account_id
+            }
+          })
+            .then(response => {
+              //console.log(response.data.data[memberDetail.account_id].global_rating);
+              memberDetail.global_rating = response.data.data[memberDetail.account_id].global_rating;
+
+              memberArray.push(memberDetail);
+              
+              if (memberArray != null) {
+                this.setState({members: memberArray});
+              }
+            })
         }
+
       });
   }
 
   render() {
-    const styles={
-      color : "#777"
+    const styles = {
+      color: "#777"
     }
 
     return (
-      <div style={styles}>         
+      <div style={styles}>
         <table className="table table-striped">
           <thead>
             <tr>
               <th scope="col">#</th>
               <th scope="col">Kullanıcı Adı</th>
-              <th scope="col">Ünvan</th>              
+              <th scope="col">Ünvan</th>
+              <th scope="col">Kişisel Rating</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.members.map((member, index) => (
-              <tr>
-                <td align="center">{index + 1}</td>
-                <td align="center">{member.account_name}</td>
-                <td align="center">{member.role_i18n}</td>
-              </tr>
-            ))}
+            {this
+              .state
+              .members
+              .map((member, index) => (
+                <tr key={member.account_id}>
+                  <td align="center">{index + 1}</td>
+                  <td align="center">{member.account_name}</td>
+                  <td align="center">{member.role_i18n}</td>
+                  <td align="center">{member.global_rating}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
